c23ecfe318b006c2f11acc6d61acd10912a4aba9
lucy2003fer <lucy48fernada@gmail.com>
2025-06-25 09:04:51 -0500

    ruta  ventas

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HerramientaController;
 use App\Http\Controllers\InsumoController;
 use App\Http\Controllers\ProduccionController;
+use App\Http\Controllers\VentaController;
 
 Route::apiResource('insumos', InsumoController::class);
 Route::apiResource('herramientas', HerramientaController::class);
-Route::apiResource('producciones', ProduccionController::class);
+Route::apiResource('produccion', ProduccionController::class);
+Route::apiResource('ventas', VentaController::class);
